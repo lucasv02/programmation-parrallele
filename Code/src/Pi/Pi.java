797076214656cccc9af7be9cf6cec68b124f787c
Lucas Vieira <contact@lucasvieira.fr>
2026-02-06 14:40:36 +0100

    Initiation des fichiers Pi.java

---
+++ Code/src/Pi/Pi.java
@@ -0,0 +1,139 @@
+package Pi;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Approximates PI using the Monte Carlo method.  Demonstrates
+ * use of Callables, Futures, and thread pools.
+ */
+
+
+/**
+ * Creates workers to run the Monte Carlo simulation
+ * and aggregates the results.
+ */
+class Master {
+
+    public long doRun(int totalCount, int numWorkers) throws InterruptedException, ExecutionException 
+    {
+
+	long startTime = System.nanoTime();
+
+	// Create a collection of tasks
+	List<Callable<Long>> tasks = new ArrayList<Callable<Long>>();
+	for (int i = 0; i < numWorkers; ++i) 
+	    {
+		tasks.add(new Worker(totalCount));
+	    }
+    
+	// Run them and receive a collection of Futures
+	ExecutorService exec = Executors.newFixedThreadPool(numWorkers);
+	List<Future<Long>> results = exec.invokeAll(tasks);
+	long total = 0;
+
+
+	// Assemble the results.
+	for (Future<Long> f : results)
+	    {
+		// Call to get() is an implicit barrier.  This will block
+		// until result from corresponding worker is ready.
+		total += f.get();
+	    }
+	double pi = 4.0 * total / totalCount / numWorkers;
+
+	long stopTime = System.nanoTime();
+
+
+	System.out.println("\nPi : " + pi );
+	System.out.println("Error: " + (Math.abs((pi - Math.PI)) / Math.PI) +"\n");
+
+	System.out.println("Ntot: " + totalCount*numWorkers);
+	System.out.println("Available processors: " + numWorkers);
+	System.out.println("Time Duration (nano secondes): " + (stopTime - startTime) + "\n");
+
+	System.out.println( (Math.abs((pi - Math.PI)) / Math.PI) +" "+ totalCount*numWorkers +" "+ numWorkers +" "+ (stopTime - startTime));
+
+	exec.shutdown();
+	return total;
+    }
+
+	public ArrayList doRunTimeErreur(int totalCount, int numWorkers) throws InterruptedException, ExecutionException
+	{
+
+		long startTime = System.nanoTime();
+
+		// Create a collection of tasks
+		List<Callable<Long>> tasks = new ArrayList<Callable<Long>>();
+		for (int i = 0; i < numWorkers; ++i)
+		{
+			tasks.add(new Worker(totalCount));
+		}
+
+		// Run them and receive a collection of Futures
+		ExecutorService exec = Executors.newFixedThreadPool(numWorkers);
+		List<Future<Long>> results = exec.invokeAll(tasks);
+		long total = 0;
+
+
+		// Assemble the results.
+		for (Future<Long> f : results)
+		{
+			// Call to get() is an implicit barrier.  This will block
+			// until result from corresponding worker is ready.
+			total += f.get();
+		}
+		double pi = 4.0 * total / totalCount / numWorkers;
+
+		long stopTime = System.nanoTime();
+
+
+//		System.out.println("\nPi : " + pi );
+//		System.out.println("Error: " + (Math.abs((pi - Math.PI)) / Math.PI) +"\n");
+//
+//		System.out.println("Ntot: " + totalCount*numWorkers);
+//		System.out.println("Available processors: " + numWorkers);
+//		System.out.println("Time Duration (nano secondes): " + (stopTime - startTime) + "\n");
+//
+//		System.out.println( (Math.abs((pi - Math.PI)) / Math.PI) +" "+ totalCount*numWorkers +" "+ numWorkers +" "+ (stopTime - startTime));
+
+		exec.shutdown();
+
+		ArrayList<Double> result = new ArrayList<>();
+		result.add(Math.abs((pi - Math.PI)) / Math.PI);
+		result.add((double)(stopTime - startTime));
+		return result;
+	}
+}
+
+/**
+ * Task for running the Monte Carlo simulation.
+ */
+class Worker implements Callable<Long> 
+{   
+    private int numIterations;
+    public Worker(int num) 
+	{ 
+	    this.numIterations = num; 
+	}
+
+  @Override
+      public Long call() 
+      {
+	  long circleCount = 0;
+	  Random prng = new Random ();
+	  for (int j = 0; j < numIterations; j++) 
+	      {
+		  double x = prng.nextDouble();
+		  double y = prng.nextDouble();
+		  if ((x * x + y * y) < 1)  ++circleCount;
+	      }
+	  return circleCount;
+      }
+}
